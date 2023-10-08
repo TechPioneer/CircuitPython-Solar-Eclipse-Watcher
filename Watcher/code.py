d23e137e56ac97ad5fe8f1143b15fdc79f08ffbc
TechPioneer <github@techpioneer.dev>
2023-10-08 14:31:10 -0500

    Refactor for better maintainability

--- Watcher/code.py
+++ Watcher/code.py
@@ -11,50 +11,102 @@ import time
 import ssl
 import busio
 import analogio
-import adafruit_as7341
-import adafruit_ltr390
 import supervisor
 import adafruit_requests
-import adafruit_mmc56x3
 from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
 
-if (config['useSHTC3']):
-    import adafruit_shtc3
+class Sensor:
+    def __init__(self, i2c, setup_func, items):
+        self.i2c = i2c
+        self.setup_func = setup_func
+        self.items = items  # This will be a list of SensorItem instances.
+        self.instance = self.setup_func(self.i2c)
+
+class SensorItem:
+    def __init__(self, mqtt_topic, read_func):
+        self.accumulated_value = 0
+        self.read_func = read_func
+        self.mqtt_topic = mqtt_topic
 
 def init():
-    global as7341
-    global ltr390
-    global mmc5603
-    
-    global mqttTopic7341
-    global mqttTopic390
-    global mqttTopic5603
-    global mqttTopicSHTC3
+    global sensors
     global sampleTime
     global mqttClient
     global io
     global feed_names
     
-    i2c0 = busio.I2C(board.GP1, board.GP0, frequency=100000)
-    #i2c1 = busio.I2C(board.GP3, board.GP2, frequency=100000)
-
-    if (config['useAS7341']):
-        as7341 = adafruit_as7341.AS7341(i2c0)
-
-    if (config['useLTR390']):
-        ltr390 = adafruit_ltr390.LTR390(i2c0)
-        
-    if (config['useMMC5603']):
-        mmc5603 = adafruit_mmc56x3.MMC5603(i2c0)
-
-    if (config['useSHTC3']):
-        global shtc3
-        shtc3 = adafruit_shtc3.SHTC3(i2c0)
-
     sampleTime = config['sampleTime']
     wifi.radio.connect(secrets['ssid'], secrets['password'])
     pool = socketpool.SocketPool(wifi.radio)
     
+    if (config['useMQTT']):
+        mqttTopic = config['mqttTopic']
+        mqttBrokerIp = config['mqttBrokerIp']
+        mqttBrokerPort = config['mqttBrokerPort']
+
+        mqttClient = MQTT.MQTT(
+            broker=mqttBrokerIp,
+            port=mqttBrokerPort,
+            socket_pool=pool,
+        )
+
+        mqttClient.will_set(mqttTopic, "OFFLINE", 1)
+        mqttClient.connect(keep_alive = sampleTime * 2 + 5)
+        
+    i2c0 = busio.I2C(board.GP1, board.GP0, frequency=100000)
+    #i2c1 = busio.I2C(board.GP3, board.GP2, frequency=100000)
+    
+    sensors = []
+
+    if (config['useAS7341']):
+        import adafruit_as7341
+        as7341_setup_func = lambda i2c: adafruit_as7341.AS7341(i2c)
+        as7341_items = [
+            SensorItem(read_func=lambda sensor: sensor.channel_clear, mqtt_topic=f"{mqttTopic}/7341/clear"),
+            SensorItem(read_func=lambda sensor: sensor.channel_nir, mqtt_topic=f"{mqttTopic}/7341/nir"),
+            SensorItem(read_func=lambda sensor: sensor.channel_415nm, mqtt_topic=f"{mqttTopic}/7341/415"),
+            SensorItem(read_func=lambda sensor: sensor.channel_445nm, mqtt_topic=f"{mqttTopic}/7341/445"),
+            SensorItem(read_func=lambda sensor: sensor.channel_480nm, mqtt_topic=f"{mqttTopic}/7341/480"),
+            SensorItem(read_func=lambda sensor: sensor.channel_515nm, mqtt_topic=f"{mqttTopic}/7341/515"),
+            SensorItem(read_func=lambda sensor: sensor.channel_555nm, mqtt_topic=f"{mqttTopic}/7341/555"),
+            SensorItem(read_func=lambda sensor: sensor.channel_590nm, mqtt_topic=f"{mqttTopic}/7341/590"),
+            SensorItem(read_func=lambda sensor: sensor.channel_630nm, mqtt_topic=f"{mqttTopic}/7341/630"),    SensorItem(read_func=lambda sensor: sensor.channel_680nm, mqtt_topic=f"{mqttTopic}/7341/680"),
+        ]
+        as7341_sensor = Sensor(i2c=i2c0, setup_func=as7341_setup_func, items=as7341_items)        
+        sensors.append(as7341_sensor)
+
+    if (config['useLTR390']):
+        import adafruit_ltr390
+        ltr390_setup_func = lambda i2c: adafruit_ltr390.LTR390(i2c)
+        ltr390_items = [
+            SensorItem(read_func=lambda sensor: sensor.uvs, mqtt_topic=f"{mqttTopic}/390/uv"),
+            SensorItem(read_func=lambda sensor: sensor.light, mqtt_topic=f"{mqttTopic}/390/light"),
+        ]
+        ltr390_sensor = Sensor(i2c=i2c0, setup_func=ltr390_setup_func, items=ltr390_items)
+        sensors.append(ltr390_sensor)
+        
+    if (config['useMMC5603']):
+        import adafruit_mmc56x3
+        mmc5603_setup_func = lambda i2c: adafruit_mmc56x3.MMC5603(i2c)
+        mmc5603_items = [
+            SensorItem(read_func=lambda sensor: sensor.magnetic[0], mqtt_topic=f"{mqttTopic}/5603/x"),
+            SensorItem(read_func=lambda sensor: sensor.magnetic[1], mqtt_topic=f"{mqttTopic}/5603/y"),
+            SensorItem(read_func=lambda sensor: sensor.magnetic[2], mqtt_topic=f"{mqttTopic}/5603/z"),
+            SensorItem(read_func=lambda sensor: sensor.temperature, mqtt_topic=f"{mqttTopic}/5603/temperature"),
+        ]
+        mmc5603_sensor = Sensor(i2c=i2c0, setup_func=mmc5603_setup_func, items=mmc5603_items)
+        sensors.append(mmc5603_sensor)
+
+    if (config['useSHTC3']):
+        import adafruit_shtc3
+        shtc3_setup_func = lambda i2c: adafruit_shtc3.SHTC3(i2c)
+        shtc3_items = [
+            SensorItem(read_func=lambda sensor: sensor.temperature, mqtt_topic=f"{mqttTopic}/shtc3/temperature"),
+            SensorItem(read_func=lambda sensor: sensor.relative_humidity, mqtt_topic=f"{mqttTopic}/shtc3/humidity"),
+        ]
+        shtc3_sensor = Sensor(i2c=i2c0, setup_func=shtc3_setup_func, items=shtc3_items)
+        sensors.append(shtc3_sensor)
+
     if (config['useAdafruitIO']):
         requests = adafruit_requests.Session(pool, ssl.create_default_context())
         io = IO_HTTP(secrets['aio_username'], secrets['aio_key'], requests)
@@ -66,72 +118,14 @@ def init():
             
         feed_names = [aio_feed_7341]
 
-    if (config['useMQTT']):
-        mqttTopic = config['mqttTopic']
-        mqttBrokerIp = config['mqttBrokerIp']
-        mqttBrokerPort = config['mqttBrokerPort']
-
-        mqttTopic7341 = f"{mqttTopic}/7341"
-        mqttTopic390 = f"{mqttTopic}/390"
-        mqttTopic5603 = f"{mqttTopic}/5603"
-        if (config['useSHTC3']):
-            mqttTopicSHTC3 = f"{mqttTopic}/shtc3"
-
-        mqttClient = MQTT.MQTT(
-            broker=mqttBrokerIp,
-            port=mqttBrokerPort,
-            socket_pool=pool,
-        )
-
-        mqttClient.will_set(mqttTopic, "OFFLINE", 1)
-        mqttClient.connect(keep_alive = sampleTime * 2 + 5)
 
 def resetVariables():
     global count
     global lastSendTime
-    global value_7341_415
-    global value_7341_445
-    global value_7341_480
-    global value_7341_515
-    global value_7341_555
-    global value_7341_590
-    global value_7341_630
-    global value_7341_680
-    global value_7341_clear
-    global value_7341_nir
     
-    global value_390_light
-    global value_390_uv
-    
-    global value_5603_x
-    global value_5603_y
-    global value_5603_z
-    global value_5603_temperature
-    
-    global value_shtc3_temperature
-    global value_shtc3_humidity
-
-    value_7341_415 = 0
-    value_7341_445 = 0
-    value_7341_480 = 0
-    value_7341_515 = 0
-    value_7341_555 = 0
-    value_7341_590 = 0
-    value_7341_630 = 0
-    value_7341_680 = 0
-    value_7341_clear = 0
-    value_7341_nir = 0
-    
-    value_390_light = 0
-    value_390_uv = 0
-    
-    value_5603_x = 0
-    value_5603_y = 0
-    value_5603_z = 0
-    value_5603_temperature = 0
-    
-    value_shtc3_temperature = 0
-    value_shtc3_humidity = 0
+    for sensor in sensors:
+        for item in sensor.items:
+            item.accumulated_value = 0    
     
     count = 0
     lastSendTime = time.time()
@@ -144,66 +138,21 @@ try:
 
         count += 1
         
-        if (config['useAS7341']):
-            value_7341_415 += as7341.channel_415nm
-            value_7341_445 += as7341.channel_445nm
-            value_7341_480 += as7341.channel_480nm
-            value_7341_515 += as7341.channel_515nm
-            value_7341_555 += as7341.channel_555nm
-            value_7341_590 += as7341.channel_590nm
-            value_7341_630 += as7341.channel_630nm
-            value_7341_680 += as7341.channel_680nm
-            value_7341_clear += as7341.channel_clear
-            value_7341_nir += as7341.channel_nir
-        
-        if (config['useLTR390']):
-            value_390_light += ltr390.uvs
-            value_390_uv += ltr390.light
-
-        if (config['useMMC5603']):
-            x, y, z = mmc5603.magnetic
-            value_5603_x += x
-            value_5603_y += y
-            value_5603_z += z
-            value_5603_temperature += mmc5603.temperature
-
-        if (config['useSHTC3']):
-            value_shtc3_temperature += shtc3.temperature
-            value_shtc3_humidity += shtc3.relative_humidity
+        for sensor in sensors:
+            for item in sensor.items:
+                item.accumulated_value += item.read_func(sensor.instance)
 
         if ((time.time() - lastSendTime) > sampleTime):
             if(count > 0):
-                if (config['useAdafruitIO']):
-                    print(f"sending {value_7341_clear}")
-                    for z in range(1):
-                        io.send_data(feed_names[z]["key"], value_7341_clear) 
+#                if (config['useAdafruitIO']):
+#                    print(f"sending {value_7341_clear}")
+#                    for z in range(1):
+#                        io.send_data(feed_names[z]["key"], value_7341_clear) 
                 
                 if (config['useMQTT']):
-                    if (config['useAS7341']):
-                        mqttClient.publish(f"{mqttTopic7341}/415", value_7341_415/count)
-                        mqttClient.publish(f"{mqttTopic7341}/445", value_7341_445/count)
-                        mqttClient.publish(f"{mqttTopic7341}/480", value_7341_480/count)
-                        mqttClient.publish(f"{mqttTopic7341}/515", value_7341_515/count)
-                        mqttClient.publish(f"{mqttTopic7341}/555", value_7341_555/count)
-                        mqttClient.publish(f"{mqttTopic7341}/590", value_7341_590/count)
-                        mqttClient.publish(f"{mqttTopic7341}/630", value_7341_630/count)
-                        mqttClient.publish(f"{mqttTopic7341}/680", value_7341_680/count)
-                        mqttClient.publish(f"{mqttTopic7341}/clear", value_7341_clear/count)
-                        mqttClient.publish(f"{mqttTopic7341}/nir", value_7341_nir/count)
-
-                    if (config['useLTR390']):
-                        mqttClient.publish(f"{mqttTopic390}/light", value_390_light/count)
-                        mqttClient.publish(f"{mqttTopic390}/uv", value_390_uv/count)
-
-                    if (config['useMMC5603']):
-                        mqttClient.publish(f"{mqttTopic5603}/x", value_5603_x/count)
-                        mqttClient.publish(f"{mqttTopic5603}/y", value_5603_y/count)
-                        mqttClient.publish(f"{mqttTopic5603}/z", value_5603_z/count)
-                        mqttClient.publish(f"{mqttTopic5603}/temperature", value_5603_temperature/count)
-
-                    if (config['useSHTC3']):
-                        mqttClient.publish(f"{mqttTopicSHTC3}/temperature", value_shtc3_temperature/count)
-                        mqttClient.publish(f"{mqttTopicSHTC3}/humidity", value_shtc3_humidity/count)
+                    for sensor in sensors:
+                        for item in sensor.items:
+                            mqttClient.publish(item.mqtt_topic, item.accumulated_value / count)
 
                 resetVariables()
 
